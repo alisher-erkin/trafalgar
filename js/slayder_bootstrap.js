504f6a654b4bf6820a57edc26cefe6007a7649f4
Alisher <alisher.e.tagaev@gmail.com>
2021-02-26 16:28:28 +0500

    move directories

---
+++ js/slayder_bootstrap.js
@@ -0,0 +1,70 @@
+<div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+  
+  <div class="carousel-inner">
+      
+    <div class="carousel-item active">
+
+    <div class="slayder_1 pt-5">
+                    <h2 class="h_slayder text-center">What our customer are saying</h2>
+                    <hr class="hr_slayder">
+                    <div class="col-sm-6 logo_text justify-content-center float-left">
+                        <img class="img_slayder_1 img-fluid" src="img/foto_slayder.svg">
+                        <p class="p_slayder_1 pt-5 pl-4">Edward Newgate<br>Founder Circle</p>
+
+                    </div>
+                    <div class="col-sm-6 slayder_text float-left">
+                        <p class="p_slayder pt-3 pl-4">“Our dedicated patient engagement app and<br> 
+                            web portal allow you to access information<br> instantaneously (no tedeous form, long calls,<br> 
+                            or administrative hassle) and securely”</p>
+                    </div>
+                </div>
+      
+    </div>
+    <div class="carousel-item">
+    <div class="slayder_1 pt-5">
+                    <h2 class="h_slayder text-center">What our customer are saying</h2>
+                    <hr class="hr_slayder">
+                    <div class="col-sm-6 logo_text justify-content-center float-left">
+                        <img class="img_slayder_1 img-fluid" src="img/foto_slayder.svg">
+                        <p class="p_slayder_1 pt-5 pl-4">Edward Newgate<br>Founder Circle</p>
+
+                    </div>
+                    <div class="col-sm-6 slayder_text float-left">
+                        <p class="p_slayder pt-3 pl-4">“Our dedicated patient engagement app and<br> 
+                            web portal allow you to access information<br> instantaneously (no tedeous form, long calls,<br> 
+                            or administrative hassle) and securely”</p>
+                    </div>
+                </div>
+    </div>
+    <div class="carousel-item">
+    <div class="slayder_1 pt-5">
+                    <h2 class="h_slayder text-center">What our customer are saying</h2>
+                    <hr class="hr_slayder">
+                    <div class="col-sm-6 logo_text justify-content-center float-left">
+                        <img class="img_slayder_1 img-fluid" src="img/foto_slayder.svg">
+                        <p class="p_slayder_1 pt-5 pl-4">Edward Newgate<br>Founder Circle</p>
+
+                    </div>
+                    <div class="col-sm-6 slayder_text float-left">
+                        <p class="p_slayder pt-3 pl-4">“Our dedicated patient engagement app and<br> 
+                            web portal allow you to access information<br> instantaneously (no tedeous form, long calls,<br> 
+                            or administrative hassle) and securely”</p>
+                    </div>
+                </div>
+    </div>
+  </div>
+ 
+</div>
+<a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+  </ol>
